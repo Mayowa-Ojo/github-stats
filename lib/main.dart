514de8e9f857f74886fdcc9e4f41a8bdbc2f63ca
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-04 20:06:03 -0700

    :tada: [init]: create starter flutter project

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false),
+    );
+
+class HomePage extends StatefulWidget {
+  // HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                child: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: NetworkImage(
+                      'https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Github",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white.withOpacity(.1)),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      hintText: 'Github username',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                padding: EdgeInsets.all(20),
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Align(
+                  child: Text(
+                    'Get Following',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
